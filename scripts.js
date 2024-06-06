155d2996169e327caebbeaf40b83a6a8eef6fc54
cleiton lahm <cleijllahm@gmail.com>
2024-06-06 14:00:57 -0300

    atualizacao no css

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-document.querySelectorAll('.navbar a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      const offset = document.querySelector('.navbar').offsetHeight; // Altura da navbar
-      const elementPosition = targetElement.getBoundingClientRect().top;
-      const offsetPosition = elementPosition + window.pageYOffset - offset;
-
-      window.scrollTo({
-          top: offsetPosition,
-          behavior: 'smooth'
-      });
-  });
-});
 
 const questions = document.querySelectorAll(".faq-question");
 
